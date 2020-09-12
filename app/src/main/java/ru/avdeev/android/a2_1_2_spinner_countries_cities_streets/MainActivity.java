8ea890e43adcae8d5dd13df7e668209d326962a6
Avdeev Aleksey <avdeev.hexer@yandex.ru>
2020-09-12 15:44:42 +0300

    Initial commit

---
+++ app/src/main/java/ru/avdeev/android/a2_1_2_spinner_countries_cities_streets/MainActivity.java
@@ -0,0 +1,14 @@
+package ru.avdeev.android.a2_1_2_spinner_countries_cities_streets;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/test/java/ru/avdeev/android/a2_1_2_spinner_countries_cities_streets/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package ru.avdeev.android.a2_1_2_spinner_countries_cities_streets;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
